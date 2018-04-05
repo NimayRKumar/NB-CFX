f20e9ab726202cc684d093dc2e6a21dd00682000
Benson <bensonkhuang@gmail.com>
2018-04-05 14:57:33 -0500

    Added SceneBuilder

---
+++ ControllerUI.java
@@ -0,0 +1,213 @@
+package assignment5;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControllerUI {
+    public Button makeBtn;
+    public Button stepBtn;
+    public Button seedBtn;
+    public Button statsBtn;
+    public Button quitBtn;
+    public Button playBtn;
+    public Button pauseBtn;
+    public ChoiceBox<String> critterBox;
+    public Slider sliderBox;
+    public Label statsBox;
+    public TextField numBox;
+    public CheckBox runstatBox;
+    public static AnimationTimer frame;
+    private ArrayList<String> files;
+    private static String myPackage;
+
+    @FXML
+    protected void initialize(){
+        frame = new AnimationTimer() {
+            private long prevStep = 0;
+
+            @Override
+            public void handle(long now) {
+                if(now - prevStep >= (long)((100_000_000))){
+                    prevStep = now;
+                    Critter.worldTimeStep();
+                    Critter.displayWorld();
+                }
+            }
+        };
+        files = new ArrayList<String>();
+        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+        getFiles();
+        initDropDown();
+    }
+
+    private void getFiles(){
+
+        File cur = new File("./src/assignment5");
+        File[] list = cur.listFiles();
+
+        for(File f : list){
+
+            if(f.isFile()){
+
+                String name = f.toString().replace(".java", "");
+                name = name.replace(cur.toString(), "");
+                name = name.substring(1);
+                files.add(name);
+            }
+        }
+    }
+
+    private void initDropDown(){
+
+        String myPackage = Critter.class.getPackage().toString().split(" ")[1];
+
+        for(String f : files){
+
+            if(f.equals("Critter")){
+                continue;
+            }
+
+            try{
+                Class o = Critter.class;
+                Class c = Class.forName(myPackage + "." + f).asSubclass(o);
+
+                critterBox.getItems().add(f);
+            }
+            catch(ClassNotFoundException | ClassCastException e){
+                continue;
+            }
+        }
+    }
+
+    public void makeCritter(){
+
+        String type = critterBox.getValue();
+        String input = numBox.getText();
+
+        int num = 0;
+
+        if(!input.equals("")){
+            try{
+                num = Integer.parseInt(input);
+            }
+            catch(NumberFormatException e){
+                System.out.println("error processing");
+            }
+        }
+        else{
+            num = 1;
+        }
+
+        while(num > 0) {
+            try {
+                Critter.makeCritter(type);
+            }
+            catch (NumberFormatException | InvalidCritterException e) {
+                System.out.println("error processing");
+            }
+
+            --num;
+        }
+
+        Critter.displayWorld();
+    }
+
+    public void worldStep(){
+
+        int num;
+        String input = numBox.getText();
+        if(!input.equals("")){
+            num = getInput(numBox);
+        }
+        else{
+            num = 1;
+        }
+
+        while(num > 0){
+
+            Critter.worldTimeStep();
+            --num;
+        }
+
+        Critter.displayWorld();
+    }
+
+    public void setSeed(){
+
+        String input = numBox.getText();
+        int num = 0;
+        try{
+            num = Integer.parseInt(input);
+            Critter.setSeed(num);
+        }
+        catch(NumberFormatException e){
+            System.out.println("error processing");
+        }
+    }
+
+    public void playWorld(){
+
+        buttonEnable(true);
+        pauseBtn.setDisable(false);
+        frame.start();
+
+        pauseBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                buttonEnable(false);
+                pauseBtn.setDisable(true);
+                System.out.println("working");
+                frame.stop();
+            }
+        });
+    }
+
+    public void buttonEnable(boolean able) {
+
+        stepBtn.setDisable(able);
+        makeBtn.setDisable(able);
+        seedBtn.setDisable(able);
+        critterBox.setDisable(able);
+        playBtn.setDisable(able);
+    }
+
+    public void updateStats(){
+        String critterName = critterBox.getValue();
+        try{
+            List<Critter> list = Critter.getInstances(critterName);
+            Class critterClass = Class.forName(myPackage + "." + critterName); //Reflection
+            Method methodCall = critterClass.getMethod("runStats", List.class);
+            methodCall.invoke(null, list);
+        }
+        catch(InvalidCritterException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | NoClassDefFoundError e){
+            System.out.println("error processing");
+        }
+    }
+
+    public void quit(){
+        System.exit(0);
+    }
+    private int getInput(TextField input){
+
+        int num = 0;
+
+        try{
+            num = Integer.parseInt(input.getText());
+        }
+        catch(NumberFormatException e){
+            System.out.println("error processing");
+        }
+
+        return num;
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,255 +1,57 @@
 package assignment5;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-
 public class Main extends Application {
-
-
-    private double width = 800;
-    private double height = 800;
-
-    private Button make = new Button("Make");
-    private Button step = new Button("Step");
-    private Button seed = new Button("Seed");
-    private Button quit = new Button("Quit");
-    private Button play = new Button("Play");
-    private Button pause = new Button("Pause");
-    private ChoiceBox<String> drop_down = new ChoiceBox<>();
-    private Slider slider = new Slider();
-
+    private double width = 600;
+    private double height = 600;
+    private double controlWidth = 327;
+    private double controlHeight = 400;
     private double tile_width = width / Params.world_width;
     private double tile_height = height / Params.world_height;
-
+    private Scene scene = new Scene(grid, width + 25,  height + 25);
     private static GridPane grid = new GridPane();
     private static GridPane tool = new GridPane();
     protected static Tile[][] world = new Tile[Params.world_width][Params.world_height];
-    private Scene scene = new Scene(grid, width + 50,  height + 50);
-    private Scene toolbox = new Scene(tool, 400, 200);
 
-	public static void main(String[] args) {
-
-	    Critter.clearWorld();
-	    launch(args);
-	}
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception { ;
+        Parent root = FXMLLoader.load(getClass().getResource("ControllerUI.fxml"));
+        Stage viewStage = new Stage();
+        initWorld();
+        viewStage.setScene(scene);
+        viewStage.setTitle("Critters GUI");
+        viewStage.show();
+        primaryStage.setScene(new Scene(root, controlWidth, controlHeight));
+        primaryStage.setTitle("ToolBox");
+        primaryStage.show();
+    }
 
-	    primaryStage.setTitle("Critters GUI");
-	    Stage control = new Stage();
-	    control.setTitle("Toolbox");
-
-	    grid.setPadding(new Insets(10, 10, 10, 10));
-	    grid.setVgap(8);
-	    grid.setHgap(10);
-
-	    initWorld();
-	    
-        TextField input = new TextField();
-        drop_down.getItems().addAll("Algae", "Craig", "Bacteria", "Vegan", "Hindu", "Mantis", "Algaephobic", "Tragic");
-        input.setMaxWidth(74);
-
-        slider.setMin(1);
-        slider.setMax(100);
-        slider.setValue(3);
-
-        GridPane.setConstraints(drop_down, 0, 1);
-        GridPane.setConstraints(input, 0, 2);
-        GridPane.setConstraints(slider, 0, 3);
-
-        GridPane.setConstraints(make, 1, 1);
-        GridPane.setConstraints(seed, 2, 1);
-        GridPane.setConstraints(play,3, 1 );
-        make.setMaxWidth(Double.MAX_VALUE);
-        seed.setMaxWidth(Double.MAX_VALUE);
-        play.setMaxWidth(Double.MAX_VALUE);
-
-        GridPane.setConstraints(step, 1, 2);
-        GridPane.setConstraints(quit,2, 2);
-        GridPane.setConstraints(pause, 3, 2);
-        step.setMaxWidth(Double.MAX_VALUE);
-        quit.setMaxWidth(Double.MAX_VALUE);
-        pause.setMaxWidth(Double.MAX_VALUE);
-
-
-        make.setOnAction(e -> makeCritter(drop_down, input));
-	     step.setOnAction(e -> worldStep(input));
-	     seed.setOnAction(e -> setSeed(input));
-	    quit.setOnAction(e -> System.exit(0));
-         play.setOnAction(e -> playWorld());
-
-        //grid.getChildren().addAll(input, drop_down, make, step, quit, seed, pause, play, slider);
-        tool.getChildren().addAll(drop_down, input, slider, make, seed, play, step, quit, pause);
-
-
-	    primaryStage.setScene(scene);
-	    control.setScene(toolbox);
-	    primaryStage.show();
-	    control.show();
+    public static void main(String[] args) {
+        Critter.clearWorld();
+        launch(args);
     }
 
     private void initWorld(){
 
-	    for(int i = 0; i < Params.world_width; ++i){
+        for(int i = 0; i < Params.world_width; ++i){
 
-	        for(int j = 0; j < Params.world_height; ++j){
+            for(int j = 0; j < Params.world_height; ++j){
 
-	            Tile tile = new Tile(i * tile_width, j * tile_height, tile_width, tile_height);
-	            grid.getChildren().add(tile);
+                Tile tile = new Tile(i * tile_width, j * tile_height, tile_width, tile_height);
+                grid.getChildren().add(tile);
 
-	            world[i][j] = tile;
+                world[i][j] = tile;
             }
         }
     }
 
-    private void makeCritter(ChoiceBox<String> drop_down, TextField input){
-
-	    String type = drop_down.getValue();
-	    switch (type){
-            case "Algae":
-                break;
-            case "Craig":
-                break;
-            case "Bacteria":
-                type = "Critter1";
-                break;
-            case "Vegan":
-                type = "Critter2";
-                break;
-            case "Hindu":
-                type = "Critter3";
-                break;
-            case "Mantis":
-                type = "Critter4";
-                break;
-            case "Algaephobic":
-                type = "AlgaephobicCritter";
-                break;
-            case "Tragic":
-                type = "TragicCritter";
-                break;
-        }
-
-	    int num;
-
-        if(!input.getText().equals("")){
-
-            num = getInput(input);
-        }
-        else{
-            num = 1;
-        }
-
-        while(num > 0) {
-
-            try {
-                Critter.makeCritter(type);
-            }
-            catch (NumberFormatException | InvalidCritterException e) {
-                System.out.println("error processing");
-            }
-
-            --num;
-        }
-
-        Critter.displayWorld();
-    }
-
-    private void worldStep(TextField input){
-
-	    int num;
-
-	    if(!input.getText().equals("")){
-
-	        num = getInput(input);
-        }
-        else{
-	        num = 1;
-        }
-
-        while(num > 0){
-
-	        Critter.worldTimeStep();
-	        --num;
-        }
-
-        Critter.displayWorld();
-    }
-
-    private void setSeed(TextField input){
-
-	    long num = getInput(input);
-	    Critter.setSeed(num);
-    }
-
-    private int getInput(TextField input){
-
-	    int num = 0;
-
-	    try{
-	        num = Integer.parseInt(input.getText());
-        }
-        catch(NumberFormatException e){
-	        System.out.println("error processing");
-        }
-
-        return num;
-    }
-
-    private void playWorld(){
-
-        AnimationTimer frame = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-
-                try{
-                    long n = (long) (500 / slider.getValue());
-                    Thread.sleep(n);
-                }
-                catch(InterruptedException e){
-                     return;
-                }
-                Critter.worldTimeStep();
-                Critter.displayWorld();
-            }
-        };
-
-        buttonEnable(true);
-        frame.start();
-
-        pause.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-                buttonEnable(false);
-                frame.stop();
-            }
-        });
-    }
-
-    private void buttonEnable(boolean able) {
-
-        step.setDisable(able);
-        make.setDisable(able);
-        seed.setDisable(able);
-        drop_down.setDisable(able);
-    }
 
 }
